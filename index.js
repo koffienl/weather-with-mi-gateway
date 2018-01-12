bedf741ddccb165ceba285d68ce44912f5f51d18
herui <herui@lashou-inc.com>
2018-01-12 18:06:03 +0800

    append log

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const baidu_apiKey = config.baidu_apiKey;
 const baidu_secretKey = config.baidu_secretKey;
 const caiyun_apiToken = config.caiyun_apiToken;
 
+const log4js = require('log4js');
+log4js.configure({
+  appenders: { app: { type: 'file', filename: 'debug.log' } },
+  categories: { default: { appenders: ['app'], level: 'debug' } }
+});
+
+const logger = log4js.getLogger('app');
+
+
 const speech = new BDSpeech(baidu_apiKey, baidu_secretKey,'mplayer', __dirname + '/tmp')
 var weather = new Weather(caiyun_apiToken);
 var audioConvert = new AudioConvert();
@@ -32,6 +41,7 @@ weather.fetch()
 })
 .then(function (res) {
     console.log('job done');    
+    logger.debug('job done.');    
 }, function(err) {
     console.log(error);    
 })
